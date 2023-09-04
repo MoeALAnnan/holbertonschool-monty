58a1021c79f73e004f6b67e6b9926e1ae0c0fbe3
MoeAlAnnan <5653@holbertonstudents.com>
2023-09-04 08:47:39 -0700

    function_getter.c

---
+++ function_getter.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ *get_function - search the function
+ *@line: char
+ *Return: func
+ */
+instruction_t *get_function(char *line)
+{
+	char *opcode;
+	instruction_t *func;
+
+	opcode = strtok(line, " \n\t\r");
+
+	func = malloc(sizeof(*func));
+	if (func == NULL)
+	{
+		fprintf(stdout, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	func->opcode = opcode;
+	func->f = NULL;
+	if (func->opcode)
+	{
+		if (strcmp(func->opcode, "push") == 0)
+			func->f = _push;
+		if (strcmp(func->opcode, "pall") == 0)
+			func->f = _pall;
+	return (func);
+	}
+}
