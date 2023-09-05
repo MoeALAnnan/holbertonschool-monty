89b3c69934e901cfd7b8a0eaf0c515f80d3e9db0
MoeAlAnnan <5653@holbertonstudents.com>
2023-09-05 06:55:32 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+/**
+ * main - main function
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{unsigned int line_number = 0;
+	char *line = NULL;
+	FILE *file_in;
+	size_t linesize_t = 0;
+	stack_t *stack = NULL;
+	instruction_t *func = NULL;
+
+	if (argc != 2)
+	{fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE); }
+	file_in = fopen(argv[1], "r");
+	if (file_in == NULL)
+	{fprintf(stderr, "error: can't open file %s\n", argv[1]);
+exit(EXIT_FAILURE); }
+	while (getline(&line, &linesize_t, file_in) != -1)
+	{line_number++;
+		func = get_function(line);
+		if ((func->opcode) == NULL)
+		{free(func);
+			if (line)
+				free(line);
+			line = NULL;
+			continue; }
+		if (func->f)
+			func->f(&stack, line_number);
+		else
+		{fprintf(stderr, "L%d: unknown instruction %s\n", line_number, func->opcode);
+			if (line)
+				free(line);
+			if (stack)
+				free_t(stack);
+			free(func);
+			fclose(file_in);
+			exit(EXIT_FAILURE); }
+		if (line)
+			free(line);
+		line = NULL;
+		free(func); }
+	if (line)
+		free(line);
+	fclose(file_in);
+	return (0); }
+
