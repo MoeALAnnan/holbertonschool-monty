650bff2d50c1b3bd3f3e2f22c94c8d3b5e60d9b4
MoeAlAnnan <5653@holbertonstudents.com>
2023-09-07 09:24:51 -0700

    add pop

--- monty.h
+++ monty.h
@@ -44,4 +44,5 @@ void free_t(stack_t *stack);
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
 #endif
